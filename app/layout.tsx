3878f5290e9c27d8906dac2370d3d20657d20fbf
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-13 22:26:39 +0530

    feat: add UnitPreferencesProvider and QueryProvider to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/providers/theme-provider"
+import { QueryProvider } from "@/providers/query-provider"
+import { UnitPreferencesProvider } from "@/hooks/use-unit-preferences"
 import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -22,8 +24,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider>
-          {children}
-          <Toaster />
+          <QueryProvider>
+            <UnitPreferencesProvider>
+              {children}
+              <Toaster />
+            </UnitPreferencesProvider>
+          </QueryProvider>
         </ThemeProvider>
       </body>
     </html>
